bc69c3846a751701f697cbfcdada242b11563866
EduardoNstm <141466697+EduardoNstm@users.noreply.github.com>
2026-04-13 09:30:59 -0300

    fix: correção da inicialização do server

---
+++ backend/server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from modules.user.user_routes import user_bp
+from modules.curriculo.curriculo_routes import curriculo_bp
+from modules import gerador_tabelas
+
+def create_app():
+    app = Flask(__name__)
+    
+    gerador_tabelas.init_db()
+    
+    app.register_blueprint(user_bp)
+    app.register_blueprint(curriculo_bp)
+    
+    return app
